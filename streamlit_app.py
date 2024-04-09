cac93b80e8c17024e37f08155642c0a5109714ae
BigTeddyRush <julian.britz@web.de>
2024-04-09 12:05:08 +0200

    connect database

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,34 @@
-import requests
-from bs4 import BeautifulSoup
-import json
-import datetime
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-
-# Benutzereingaben für Start- und Enddatum erhalten
-start_date = '2024-02-01'
-end_date = '2024-02-01'
-
-# URL mit Benutzereingaben für Start- und Enddatum generieren
-siteurl = f"https://api.energy-charts.info/total_power?country=de&start={start_date}T00%3A00%2B01%3A00&end={end_date}T23%3A45%2B01%3A00"
-
-print("Generierte URL:", siteurl)
-
-# power consumption in Germany on one specific day
-#siteurl = "https://api.energy-charts.info/total_power?country=de&start=2023-01-01T00%3A00%2B01%3A00&end=2023-01-01T23%3A45%2B01%3A00"
-response = requests.get(siteurl)
-power_consumption = json.loads(response.content.decode('utf-8'))
-
-# create a list of the time_stamps, types and data
-unix_seconds_list = []
-name_list = []
-data_list = []
-for typ in range(len(power_consumption['production_types'])):
-    for time in range(len(power_consumption['unix_seconds'])):
-        # append unix_seconds
-        unix_seconds_list.append(datetime.datetime.fromtimestamp(power_consumption['unix_seconds'][time]).strftime("%m/%d/%Y, %H:%M:%S"))
-        # append production_types
-        name_list.append(power_consumption['production_types'][typ]['name'])
-        # append data
-        data_list.append(power_consumption['production_types'][typ]['data'][time])
+import os
+import pyodbc
 
 # create dict
-power_consumption_dict = {'unix_seconds' : unix_seconds_list, 'name' : name_list, 'data' : data_list}
+power_consumption_dict = {'unix_seconds' : [], 'name' : [], 'data' : []}
+
+server = 'energycharts.database.windows.net'
+database = 'EnergyChartsDB'
+driver = '{ODBC Driver 17 for SQL Server}'
+username = 'bigteddyrush'
+password = os.environ['PASSWORD']
+
+# Daten aus Datenbank abrufen
+try:
+    with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+        with conn.cursor() as cursor:
+            # Daten aus der Datenbank abrufen
+            cursor.execute("SELECT unix_seconds, name, data FROM dbo.EnergyCharts")
+            rows = cursor.fetchall()
+            
+            # Werte in das Dictionary speichern
+            for row in rows:
+                unix_seconds, name, data = row
+                power_consumption_dict['unix_seconds'].append(unix_seconds)
+                power_consumption_dict['name'].append(name)
+                power_consumption_dict['data'].append(data)
+except pyodbc.Error as ex:
+    print("Fehler beim Verbinden zur Datenbank:", ex)
 
 ################################
 
